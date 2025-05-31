3f293dfb68e6be2baee7c066b8a1a0f7939b92a2
takuya wakazono <takuyawakazono@takuyanoMacBook-Pro.local>
2025-05-31 16:32:55 +0900

    add profile page

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,11 @@
+export default function ProfilePage() {
+  return (
+    <main>
+      <h1>takuyawww</h1>
+      <ul>
+        <li>1995/11/01</li>
+        <li>Fukuoka/Japan</li>
+      </ul>
+    </main>
+  );
+} 
